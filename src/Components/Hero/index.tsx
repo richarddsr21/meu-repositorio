2df6cf850d76faae40907046757d381ed6d88659
Richard Reis <richarddsr21@gmail.com>
2024-06-09 13:47:50 -0300

    Home pronta

---
+++ src/Components/Hero/index.tsx
@@ -0,0 +1,65 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export const Hero = () => {
+  return (
+    <div className="w-full h-full">
+      <div className="w-full flex flex-col justify-center items-center lg:hidden">
+        <img
+          className="mt-16 rounded-full shadow-primary shadow-md lg:"
+          src="/image/image-hero/minha-foto.jpg"
+          alt="Minha Imagem"
+          width={200}
+          height={200}
+        />
+        <div className="mt-5 flex text-xl">
+          <FaGithub className="mr-4" />
+          <FaLinkedin />
+        </div>
+        <div className="pt-12">
+          <h1 className="text-2xl font-bold bg-primary px-3 py-1 rounded-lg">
+            Desenvolvedor FrontEnd
+          </h1>
+        </div>
+        <div className="pt-4">
+          <span className="flex text-justify px-7 max-w-xl">
+            Meu nome é Richard da Silva Reis, tenho 21 aos e sou desenvolvedor
+            FrontEnd com 3 meses de experiência em criar websites responsivos e
+            amigáveis. Atualmente estou trabalhando com React, Next e
+            TailwindCSS
+          </span>
+        </div>
+      </div>
+
+      {/* Mobile */}
+
+      <div className="w-full flex justify-between items-center max-lg:hidden">
+        <div className="max-w-[500px]">
+          <div className="flex justify-center pt-12">
+            <h1 className="text-4xl font-bold bg-primary px-3 py-1 rounded-lg">
+              Desenvolvedor FrontEnd
+            </h1>
+          </div>
+          <div className="pt-4">
+            <span className="flex text-justify px-7">
+              Meu nome é Richard da Silva Reis, tenho 21 aos e sou desenvolvedor
+              FrontEnd com 3 meses de experiência em criar websites responsivos
+              e amigáveis. Atualmente estou trabalhando com React, Next e
+              TailwindCSS
+            </span>
+          </div>
+          <div className="mt-5 ml-7 flex text-xl">
+            <FaGithub className="mr-4" />
+            <FaLinkedin />
+          </div>
+        </div>
+        <img
+          className="mt-16 rounded-full shadow-primary shadow-md lg:"
+          src="/image/image-hero/minha-foto.jpg"
+          alt="Minha Imagem"
+          width={350}
+          height={350}
+        />
+      </div>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Header } from "./Components/header";
+import { Hero } from "./Components/Hero";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <div className="mx-auto md:p-5 md:max-w-[1056px] flex justify-center">
+  <div className="md:max-w-[1056px] h-full mx-auto md:p-5 flex flex-col justify-center drop-shadow-lg pb-12">
     <React.StrictMode>
       <Header />
+      <Hero />
     </React.StrictMode>
   </div>
 );
